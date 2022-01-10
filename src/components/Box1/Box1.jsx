8ce3cef323fc007d5d0909a44e1e921aa7701841
Mithila91 <Mithila91@users.noreply.github.com>
2022-01-10 13:04:21 +0100

    [WIP]First animation added and applied with state

--- src/components/Box1/Box1.jsx
+++ src/components/Box1/Box1.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { motion } from 'framer-motion'
 
 export default function Box1() {
+  const [isAnimating, setIsAnimating] = useState(false)
   return (
     <div className="box-container">
-      <div className="box"></div>
+      <motion.div
+        className="box"
+        animate={{
+          x: isAnimating ? 700 : 0,
+          opacity: isAnimating ? 1 : 0.5,
+          rotate: isAnimating ? 360 : 0,
+        }}
+        initial={{
+          opacity: 0.1,
+        }}
+        transition={{
+          type: 'spring',
+          stiffness: 100,
+          damping: 100,
+        }}
+        onClick={() => setIsAnimating(!isAnimating)}
+      ></motion.div>
     </div>
   )
 }
